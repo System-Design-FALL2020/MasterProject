35dd6d9410fd2ae77792a07383c15a8f14c8cf5c
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-22 04:12:10 -0400

    Errors serving Static pages, need fix swup

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// Create Express
-
 const express = require('express');
+const { restart } = require('nodemon');
+const { dirname } = require('path');
 const app = express();
 // Create Path
 
 const path = require('path');
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'Views')));
+// app.use(express.static(path.join(__dirname, 'Views')));
+
+app.use(express.static('public'));
+
+
+
+
 
 // ROUTES
 
@@ -17,21 +23,6 @@ app.use(express.static(path.join(__dirname, 'Views')));
 
 // Create the PORT
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// // Create Seqelize
-// const Sequelize = require('sequelize');
-// const sqlConnection = new Sequelize('atomic_schema','root','password', {
-//     dialect: 'sqlite',
-//     storage: '/Database/database.sqlite'
-// })
-
-// let User = connection.define('user', {
-//     first_name: Sequelize.STRING,
-//     last_name: Sequelize.STRING,
-//     email: Sequelize.STRING,
-// })
-
-// connection.sync();
