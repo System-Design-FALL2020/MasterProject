d4b49741a8326b2cd31171b2279d5c26bd6d141d
JGustafsonIII <jgustafsoniii@github.com>
2020-10-24 17:40:03 -0400

    Routing setup and public directly set

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res) => {
+    res.redirect('/Home')
+})
+
+router.get('/Home', (req, res) => {
     res.sendFile('/views/home/index.html', { root: 'public'});
+})
+
+router.get('/Browse', (req,res) => {
+    res.sendFile('/views/browse/browse.html', { root: 'public'});
+})
+
+router.get('/Login', (req,res) => {
+    res.sendFile('/views/login/login.html', { root: 'public'});
 })
 
 module.exports = router;
