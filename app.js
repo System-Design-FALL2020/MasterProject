95cd657d72ef134fc9a22af037ff4746e8dce29c
JGustafsonIII <jgustafsoniii@github.com>
2020-10-24 17:30:01 -0400

    Fixed routing to index page

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
+const router = require('./routes/routes')
 const path = require('path');
 const app = express();
-// Create Path
 
 
+app.use(express.static(__dirname + "/public"));
+// app.use("/backgrounds", express.static(__dirname + "public/content/backgrounds"));
+// app.use("/css", express.static(__dirname + "public/content/css"));
+// app.use("/js", express.static(__dirname + "public/content/js"));
+// app.use("/img", express.static(__dirname + "public/content/img"));
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req,res) => {
-
-    res.sendFile('./public/Views/Home/index.html', { root: __dirname});
-
-})
-
-// Create the PORT
+app.use(router);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
--- public/content/js/login.js
+++ public/content/js/login.js
@@ -12,6 +12,3 @@ container.classList.add('right-panel-active'));
 
 signInButton.addEventListener('click', () =>
 container.classList.remove('right-panel-active'));
-
-
-
